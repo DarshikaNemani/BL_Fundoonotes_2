b7dcad35088dbcdc9057152123be34082b33771c
DarshikaNemani <dk5673@srmist.edu.in>
2025-06-05 10:50:51 +0530

    [Darshika]Ref: navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// src/app/components/navbar/navbar.component.ts
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  @Output() menuClicked = new EventEmitter<void>();
+  @Output() logoutClicked = new EventEmitter<void>();
 
+  onMenuClick() {
+    this.menuClicked.emit();
+  }
+
+  onLogout() {
+    this.logoutClicked.emit();
+  }
 }
