642f92d93c66b87dc66e5963ba921fa824470716
DarshikaNemani <dk5673@srmist.edu.in>
2025-06-06 19:00:46 +0530

    [Darshika]Add:Made changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ArchiveComponent } from './pages/archive/archive.component';
+import { TrashComponent } from './pages/trash/trash.component';
 import { AuthGuardService } from './services/authGuard_service/auth-guard.service';
 
 export const routes: Routes = [
@@ -14,8 +16,23 @@ export const routes: Routes = [
     component: LoginComponent,
   },
   {
-    path: '',
+    path: 'notes',
     component: HomeComponent,
-    canActivate:[AuthGuardService],
+    canActivate: [AuthGuardService],
+  },
+  {
+    path: 'archive',
+    component: ArchiveComponent,
+    canActivate: [AuthGuardService],
+  },
+  {
+    path: 'trash',
+    component: TrashComponent,
+    canActivate: [AuthGuardService],
+  },
+  {
+    path: '',
+    redirectTo: '/notes',
+    pathMatch: 'full'
   },
 ];
